d79c486c1da9e7cd7b1b75df008b6e114478e09b
Victor Huang <i@imvictor.tech>
2018-04-29 12:52:54 +0800

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,8 +9,7 @@ rm /usr/sbin/screen
 rm /usr/bin/nohup
 rm /root/Drcom*
 rm /etc/drcom.conf
-echo "# Put your custom commands here that should be executed once" > /etc/rc.local
-echo "# the system init finished. By default this file does nothing." >> /etc/rc.local
-echo >> /etc/rc.local
+rm /etc/rc.local
+mv /etc/rc.local.hiwifi_backup /etc/rc.local
 read -p "Base removed. Would you like to remove bash? [Return/Ctrl+C]"
 rm /bin/bash /bin/rbash
