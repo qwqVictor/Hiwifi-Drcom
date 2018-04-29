41cc0e1eb8787e8dfa532aad2c0f66f1d041d26e
Victor Huang <i@imvictor.tech>
2018-04-29 12:48:20 +0800

    improved rc.local modification

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+printf '__  ___          _ _____       ____                          \n   / / / (_)      __(_) __(_)     / __ \______________  ____ ___ \n  / /_/ / / | /| / / / /_/ /_____/ / / / ___/ ___/ __ \/ __ `__ \\n / __  / /| |/ |/ / / __/ /_____/ /_/ / /  / /__/ /_/ / / / / / /\n/_/ /_/_/ |__/|__/_/_/ /_/     /_____/_/   \___/\____/_/ /_/ /_/ \n'
 cp -R Package/python/usr /
 rm /usr/bin/python
 ln -s /usr/bin/python2.7 /usr/bin/python
@@ -8,7 +9,8 @@ ln -s /bin/bash /bin/drsh
 cp Package/drcom/Drcom.py /root/
 cp Package/drcom/Drcom.sh /root/
 cp Package/drcom/drcom.conf /etc/
-cp Package/boot/rc.local /etc/rc.local
+mv /etc/rc.local /etc/rc.local.hiwifi_backup
+cat Package/boot/rc.local /etc/rc.local.hiwifi_backup > /etc/rc.local
 read -p "Username: " username
 read -p "Password: " -s password
 echo
